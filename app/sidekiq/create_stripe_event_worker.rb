a3ef3ba744c06c19ca98ec7d0db0af09ceb0e0f2
Mario Miklos <mario.miklos@smartpension.co.uk>
2024-02-20 17:21:34 +0100

    Change worker to call new unit instead of containing logic

--- app/sidekiq/create_stripe_event_worker.rb
+++ app/sidekiq/create_stripe_event_worker.rb
@@ -6,14 +6,15 @@ class CreateStripeEventWorker
 
   def perform(request_body, stripe_signature)
     build_stripe_event(request_body, stripe_signature)
-    CreateStripeEvent::EntryPoint.call(
-      {
-        stripe_event_id: stripe_event['id'],
-        event_type: stripe_event['type'],
-        event_json: stripe_event['data']['object'].to_json
-      }
-    )
-    dispatch_event
+    CreateStripeEvent::EntryPoint.call({
+                                         stripe_event_id: stripe_event['id'],
+                                         event_type: stripe_event['type'],
+                                         event_json: stripe_event['data']['object'].to_json
+                                       })
+    DispatchStripeEvent::EntryPoint.call({
+                                           stripe_object: stripe_event['data']['object'],
+                                           event_type: stripe_event['type']
+                                         })
   end
 
   private
@@ -26,58 +27,5 @@ class CreateStripeEventWorker
       stripe_signature,
       Rails.application.credentials.webhook_signing_key
     )
-  end
-
-  def dispatch_event
-    # TODO: create a unit to handle this logic
-    stripe_object = stripe_event['data']['object']
-    case stripe_event['type']
-    when 'customer.created'
-      CreateUser::EntryPoint.call(user_pFarams(stripe_object))
-    when 'customer.subscription.created'
-      CreateSubscription::EntryPoint.call(subscription_params(stripe_object))
-    when 'invoice.created'
-      CreateInvoice::EntryPoint.call(created_invoice_params(stripe_object))
-    when 'invoice.paid'
-      UpdateInvoice::EntryPoint.call(state_changed_invoice_params(stripe_object))
-      UpdateSubscription::EntryPoint.call(stripe_subscription_id: stripe_object['subscription'], state: 'paid')
-    when 'invoice.finalized'
-      UpdateInvoice::EntryPoint.call(state_changed_invoice_params(stripe_object))
-    when 'customer.subscription.deleted'
-      UpdateSubscription::EntryPoint.call(stripe_subscription_id: stripe_object['id'], state: 'canceled')
-    end
-  end
-
-  def user_params(stripe_customer)
-    {
-      name: stripe_customer['name'],
-      email: stripe_customer['email'],
-      stripe_customer_id: stripe_customer['id']
-    }.compact_blank
-  end
-
-  def subscription_params(stripe_subscription)
-    {
-      stripe_customer_id: stripe_subscription['customer'],
-      stripe_subscription_id: stripe_subscription['id'],
-      plan_name: stripe_subscription['items']['data'][0]['plan']['nickname'],
-      start_date: stripe_subscription['start_date']
-    }.compact_blank
-  end
-
-  def created_invoice_params(stripe_invoice)
-    {
-      stripe_customer_id: stripe_invoice['customer'],
-      stripe_invoice_id: stripe_invoice['id'],
-      stripe_subscription_id: stripe_invoice['subscription'],
-      amount_due: stripe_invoice['amount_due']
-    }.compact_blank
-  end
-
-  def state_changed_invoice_params(stripe_invoice)
-    {
-      stripe_invoice_id: stripe_invoice['id'],
-      state: stripe_invoice['status']
-    }.compact_blank
   end
 end
--- spec/sidekiq/create_stripe_event_worker_spec.rb
+++ spec/sidekiq/create_stripe_event_worker_spec.rb
@@ -1,11 +1,37 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
 
 RSpec.describe CreateStripeEventWorker, type: :job do
   let(:event) { double('event') }
+  let(:data_double) { double('data') }
+  let(:event_data) { { 'data' => { 'object' => data_double } } }
+  let(:request_body) { 'request_body' }
+  let(:stripe_signature) { 'stripe_signature' }
+
+  after { described_class.clear }
 
   it 'enqueues the job' do
-    expect { described_class.perform_later(event) }.to have_enqueued_job(described_class)
+    expect { described_class.perform_async(request_body, stripe_signature) }
+      .to change(described_class.jobs, :size).by(1)
+  end
+
+  context 'when the Stripe event is valid' do
+    before do
+      allow(Stripe::Webhook).to receive(:construct_event).and_return(event)
+      allow(event).to receive(:[]).with('id').and_return('event_id')
+      allow(event).to receive(:[]).with('type').and_return('event_type')
+      allow(event).to receive(:[]).with('data').and_return(event_data)
+    end
+
+    it 'calls the correct units' do
+      expect(CreateStripeEvent::EntryPoint).to receive(:call).once
+      expect(DispatchStripeEvent::EntryPoint).to receive(:call).once
+
+      described_class.perform_async(request_body, stripe_signature)
+      described_class.drain
+    end
   end
 end
