cf8670f50d805b10996c56cacedf56a19a2a7044
Mario Miklos <mario.miklos@smartpension.co.uk>
2024-02-20 17:25:33 +0100

    Fix DispatchStripeEvent actions pec

--- spec/units/dispatch_stripe_event/action_spec.rb
+++ spec/units/dispatch_stripe_event/action_spec.rb
@@ -10,66 +10,61 @@ RSpec.describe DispatchStripeEvent::Action do
 
   context 'when event type is customer.created' do
     let(:event_type) { 'customer.created' }
-    let(:stripe_object) { { name: 'John', email: 'john@mail.com', stripe_customer_id: 'stripe_id' } }
+    let(:stripe_object) { { name: 'John', email: 'john@mail.com', id: 'stripe_id' } }
 
     it 'calls CreateUser' do
-      expect(CreateUser::EntryPoint).to receive(:call)
+      expect(CreateUser::EntryPoint).to receive(:call).once
       subject
     end
   end
 
   context 'when event type is customer.subscription.created' do
     let(:event_type) { 'customer.subscription.created' }
-    let(:stripe_object) do
-      { stripe_customer_id: 'customer_id', stripe_subscription_id: 'sub_id', start_date: Time.zone.today }
-    end
+    let(:stripe_object) { { customer: 'customer_id', id: 'sub_id', start_date: Time.zone.today } }
 
     it 'calls CreateSubscription' do
-      expect(CreateSubscription::EntryPoint).to receive(:call)
+      expect(CreateSubscription::EntryPoint).to receive(:call).once
       subject
     end
   end
 
   context 'when event type is customer.subscription.deleted' do
     let(:event_type) { 'customer.subscription.deleted' }
-    let(:stripe_object) { { stripe_subscription_id: 'sub_id', state: 'canceled' } }
+    let(:stripe_object) { { id: 'sub_id', state: 'canceled' } }
 
     it 'calls UpdateSubscription' do
-      expect(UpdateSubscription::EntryPoint).to receive(:call)
+      expect(UpdateSubscription::EntryPoint).to receive(:call).once
       subject
     end
   end
 
   context 'when event type is invoice.created' do
     let(:event_type) { 'invoice.created' }
-    let(:stripe_object) do
-      { stripe_customer_id: 'cus_id', stripe_invoice_id: 'inv_id',
-        stripe_subscription_id: 'sub_id' }
-    end
+    let(:stripe_object) { { customer: 'cus_id', id: 'inv_id', subscription: 'sub_id' } }
 
     it 'calls CreateInvoice' do
-      expect(CreateInvoice::EntryPoint).to receive(:call)
+      expect(CreateInvoice::EntryPoint).to receive(:call).once
       subject
     end
   end
 
   context 'when event type is invoice.paid' do
     let(:event_type) { 'invoice.paid' }
-    let(:stripe_object) { { id: 'inv_id', state: 'paid', subscription: 'sub_id' } }
+    let(:stripe_object) { { id: 'inv_id', status: 'paid', subscription: 'sub_id' } }
 
     it 'calls UpdateInvoice and UpdateSubscription' do
-      expect(UpdateInvoice::EntryPoint).to receive(:call)
-      expect(UpdateSubscription::EntryPoint).to receive(:call)
+      expect(UpdateInvoice::EntryPoint).to receive(:call).once
+      expect(UpdateSubscription::EntryPoint).to receive(:call).once
       subject
     end
   end
 
   context 'when event type is invoice.finalized' do
     let(:event_type) { 'invoice.finalized' }
-    let(:stripe_object) { { id: 'inv_id', state: 'paid' } }
+    let(:stripe_object) { { id: 'inv_id', status: 'paid' } }
 
     it 'calls UpdateInvoice' do
-      expect(UpdateInvoice::EntryPoint).to receive(:call)
+      expect(UpdateInvoice::EntryPoint).to receive(:call).once
       subject
     end
   end
